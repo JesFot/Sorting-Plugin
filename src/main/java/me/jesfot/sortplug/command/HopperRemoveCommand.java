2ccefc13258a39debe3ca7b4cb14bb57571c38e5
JesFot <jesfot@gmail.com>
2015-05-27 18:50:03 +0200

    Up

--- src/main/java/me/jesfot/sortplug/command/HopperRemoveCommand.java
+++ src/main/java/me/jesfot/sortplug/command/HopperRemoveCommand.java
@@ -1,6 +1,5 @@
 package me.jesfot.sortplug.command;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import me.jesfot.sortplug.SortPlug;
@@ -15,10 +14,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class HopperAddCommand implements CommandExecutor
+public class HopperRemoveCommand implements CommandExecutor
 {
 	private SortPlug sp;
-	private String usageMessage = "Usage : /addfilter [x, y, z] <itemType>";
+	private String usageMessage = "Usage : /removefilter [x, y, z] <itemType>";
 	
 	private Material assigna(boolean b)
 	{
@@ -26,18 +25,17 @@ public class HopperAddCommand implements CommandExecutor
 		return null;
 	}
 	
-	public HopperAddCommand(SortPlug p_sp)
+	public HopperRemoveCommand(SortPlug p_sp)
 	{
 		this.sp = p_sp;
 	}
 	
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args)
 	{
-		this.sp.getConfig().reloadCustomConfig();
-		if(cmd.getName().equalsIgnoreCase("addfilter"))
+		if(cmd.getName().equalsIgnoreCase("removefilter"))
 		{
-			if(args.length == 1) // If the player didn't give coordinates
+			if(args.length == 1)
 			{
 				if(!(sender instanceof Player)) // The console cannot click anywhere
 				{
@@ -54,11 +52,11 @@ public class HopperAddCommand implements CommandExecutor
 					return true;
 				}
 				player.sendMessage("Do a left-click on the hooper you want to configure.");
-				HopperComListener listener = new HopperComListener(player, this.sp, material);
+				HopperComListener listener = new HopperComListener(player, this.sp, material, false);
 				this.sp.getServer().getPluginManager().registerEvents(listener, this.sp.getPlugin());
 				return true;
 			}
-			else if(args.length == 4) // If the player (or console) give coordinates
+			else if(args.length == 4)
 			{
 				String itype = args[3]; // Get the item type
 				World world = null;
@@ -73,15 +71,12 @@ public class HopperAddCommand implements CommandExecutor
 				}
 				// Create the given location
 				Location loc = new Location(world, Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]));
-				if(!(loc.getBlock().getType().equals(Material.HOPPER))) // If we don't have an hopper, cancel.
-				{
-					sender.sendMessage(ChatColor.RED + "Please select an hopper with the coordonates.");
-					return true;
-				}
 				if(!this.sp.getConfig().getCustomConfig().contains("filters.ids")) // Verify that we are not on the first ID
 				{
 					this.sp.getConfig().getCustomConfig().createSection("filters.ids");
 					this.sp.getConfig().getCustomConfig().set("filters.ids", 0);
+					sender.sendMessage("This location was no filters.");
+					return true;
 				}
 				int ids = this.sp.getConfig().getCustomConfig().getInt("filters.ids"); // Get the total ids
 				int id = -1;
@@ -96,19 +91,19 @@ public class HopperAddCommand implements CommandExecutor
 				}
 				if(id == -1)
 				{
-					id = ids; // If we don't already have this location stored, store a new one
+					sender.sendMessage("This location was no filters.");
+					return true;
 				}
-				this.sp.getConfig().storeLoc("filters.hoppers.id"+id+".location", loc); // Save the location at this id
-				this.sp.getConfig().getCustomConfig().set("filters.ids", (id+1)); // Store the id+1 for the next registration
 				if(!this.sp.getConfig().getCustomConfig().contains("filters.hoppers.id"+id+".items"))
 				{
-					this.sp.getConfig().getCustomConfig().createSection("filters.hoppers.id"+id+".items");
-					this.sp.getConfig().getCustomConfig().set("filters.hoppers.id"+id+".items", new ArrayList<String>()); // If there is no list, create one
+					sender.sendMessage("Good..., no items to this location.");
+					this.sp.getConfig().getCustomConfig().set("filters.hoopers.id"+id, null);
+					return true;
 				}
 				List<String> filter = this.sp.getConfig().getCustomConfig().getStringList("filters.hoppers.id"+id+".items");
-				if(!filter.contains(itype))
+				if(filter.contains(itype))
 				{
-					filter.add(itype); // Add the given material to the list
+					filter.remove(itype); // Remove the given material to the list
 				}
 				this.sp.getConfig().getCustomConfig().set("filters.hoppers.id"+id+".items", filter);
 				this.sp.getConfig().saveCustomConfig(); // And save data.
@@ -117,8 +112,6 @@ public class HopperAddCommand implements CommandExecutor
 			sender.sendMessage(ChatColor.RED + this.usageMessage);
 			return true;
 		}
-		sender.sendMessage(ChatColor.RED + this.usageMessage);
-		return true;
+		return false;
 	}
-
 }
--- src/main/java/me/jesfot/sortplug/listeners/HopperComListener.java
+++ src/main/java/me/jesfot/sortplug/listeners/HopperComListener.java
@@ -20,10 +20,20 @@ public class HopperComListener implements Listener
 	private Player player;
 	private SortPlug sp;
 	private Config conf;
+	private boolean add;
 	private Material mat;
 	
 	public HopperComListener(Player p_player, SortPlug p_sp, Material material)
 	{
+		this.add = true;
+		this.player = p_player;
+		this.sp = p_sp;
+		this.conf = this.sp.getConfig();
+		this.mat = material;
+	}
+	public HopperComListener(Player p_player, SortPlug p_sp, Material material, boolean add)
+	{
+		this.add = add;
 		this.player = p_player;
 		this.sp = p_sp;
 		this.conf = this.sp.getConfig();
@@ -31,8 +41,12 @@ public class HopperComListener implements Listener
 	}
 	
 	@EventHandler
-	public void onPlayerClick(final PlayerInteractEvent event)
+	public void onPlayerClickAdd(final PlayerInteractEvent event)
 	{
+		if(!add)
+		{
+			return;
+		}
 		Action action = event.getAction();
 		Action leftClickBlock = Action.LEFT_CLICK_BLOCK;
 		if(event.getPlayer() == this.player)
@@ -85,6 +99,58 @@ public class HopperComListener implements Listener
 				{
 					this.player.sendMessage("You didn't click on a hopper, try again.");
 				}
+				PlayerInteractEvent.getHandlerList().unregister(this);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerClickRemove(final PlayerInteractEvent event)
+	{
+		Player player2 = event.getPlayer();
+		if(player2.equals(this.player))
+		{
+			if(event.getAction().equals(Action.LEFT_CLICK_BLOCK))
+			{
+				String itype = this.mat.name(); // Get the item type
+				// Create the given location
+				Location loc = event.getClickedBlock().getLocation();
+				if(!this.sp.getConfig().getCustomConfig().contains("filters.ids")) // Verify that we are not on the first ID
+				{
+					this.sp.getConfig().getCustomConfig().createSection("filters.ids");
+					this.sp.getConfig().getCustomConfig().set("filters.ids", 0);
+					player2.sendMessage("This location was no filters.");
+					return;
+				}
+				int ids = this.sp.getConfig().getCustomConfig().getInt("filters.ids"); // Get the total ids
+				int id = -1;
+				for(int i=0; i<ids; i++)
+				{
+					Location loca = this.sp.getConfig().getLoc("filters.hoppers.id"+i+".location");
+					if(loca != null && loc.equals(loca))
+					{
+						id = ids;
+						break;
+					}
+				}
+				if(id == -1)
+				{
+					player2.sendMessage("This location was no filters.");
+					return;
+				}
+				if(!this.sp.getConfig().getCustomConfig().contains("filters.hoppers.id"+id+".items"))
+				{
+					player2.sendMessage("Good..., no items to this location.");
+					this.sp.getConfig().getCustomConfig().set("filters.hoopers.id"+id, null);
+					return;
+				}
+				List<String> filter = this.sp.getConfig().getCustomConfig().getStringList("filters.hoppers.id"+id+".items");
+				if(filter.contains(itype))
+				{
+					filter.remove(itype); // Remove the given material to the list
+				}
+				this.sp.getConfig().getCustomConfig().set("filters.hoppers.id"+id+".items", filter);
+				this.sp.getConfig().saveCustomConfig(); // And save data.
 				PlayerInteractEvent.getHandlerList().unregister(this);
 			}
 		}
